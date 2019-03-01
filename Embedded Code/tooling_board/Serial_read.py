b04c6eb4e665ce4a9739b06af9791d69a024c0c0
dshdrover <dshdrover@mun.ca>
2019-03-01 12:50:24 -0330

    created a serial read program
    created a serial read program for depth and pressure sensors values

---
+++ Embedded Code/tooling_board/Serial_read.py
@@ -0,0 +1,15 @@
+import serial
+import datetime
+
+ser = serial.Serial('COM11', 115200)
+
+while True:
+    ser_bytes = ser.readline()
+    decoded_bytes = ser_bytes[2:len(ser_bytes)-4].decode("utf-8")
+    currentDT = datetime.datetime.now()
+    test = str(currentDT)
+    print(test + ": " + decoded_bytes)
+    x = decoded_bytes.split(',')
+    currentDT = datetime.datetime.now()
+    test = str(currentDT)
+    print(test + ": " + x[0])
