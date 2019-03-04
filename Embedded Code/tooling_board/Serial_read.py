2212132b1859cce66ceba25c648e649343155d34
dshdrover <dshdrover@mun.ca>
2019-03-04 10:41:05 -0330

    updated test code

--- Embedded Code/tooling_board/Serial_read.py
+++ Embedded Code/tooling_board/Serial_read.py
@@ -5,7 +5,7 @@ ser = serial.Serial('COM11', 115200)
 
 while True:
     ser_bytes = ser.readline()
-    decoded_bytes = ser_bytes[2:len(ser_bytes)-4].decode("utf-8")
+    decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")
     currentDT = datetime.datetime.now()
     test = str(currentDT)
     print(test + ": " + decoded_bytes)
